582b6d8dbf2fbffbb7bf1de1dca3c49f772ef898
jeduardofr <jeduardo.fuentes@alumnos.udg.mx>
2022-12-02 23:24:10 -0600

    feat: day 2 part 2

--- day_2/src/main.rs
+++ day_2/src/main.rs
@@ -36,28 +36,59 @@ fn main() -> io::Result<()> {
                 let (op, mine) = sanitized.split_at(index);
 
                 let opponent_move = op.trim();
-                let my_secret_move = mine.trim();
+                let what_should_i_do = mine.trim();
+                let mut my_secret_move = String::new();
 
                 let vs_score: u32 = match opponent_move {
-                    "A" => match my_secret_move {
-                        "Y" => 6,
-                        "X" => 3,
+                    "A" => match what_should_i_do {
+                        "X" => {
+                            my_secret_move = String::from("Z");
+                            0
+                        },
+                        "Y" => {
+                            my_secret_move = String::from("X");
+                            3
+                        },
+                        "Z" => {
+                            my_secret_move = String::from("Y");
+                            6
+                        },
                         _ => 0,
                     },
-                    "B" => match my_secret_move {
-                        "Z" => 6,
-                        "Y" => 3,
+                    "B" => match what_should_i_do {
+                        "X" => {
+                            my_secret_move = String::from("X");
+                            0
+                        },
+                        "Y" => {
+                            my_secret_move = String::from("Y");
+                            3
+                        },
+                        "Z" => {
+                            my_secret_move = String::from("Z");
+                            6
+                        },
                         _ => 0,
                     },
-                    "C" => match my_secret_move {
-                        "X" => 6,
-                        "Z" => 3,
+                    "C" => match what_should_i_do {
+                        "X" => {
+                            my_secret_move = String::from("Y");
+                            0
+                        },
+                        "Y" => {
+                            my_secret_move = String::from("Z");
+                            3
+                        },
+                        "Z" => {
+                            my_secret_move = String::from("X");
+                            6
+                        },
                         _ => 0,
                     },
                     _ => 0,
                 };
 
-                let move_score: u32 = match my_secret_move {
+                let move_score: u32 = match my_secret_move.as_ref() {
                     "Y" => 2,
                     "X" => 1,
                     "Z" => 3,
